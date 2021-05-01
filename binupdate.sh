3ac4fa342c7896dbbb09db851d5c8bcdd948540e
root <root@pi-star>
2021-05-01 12:42:09 -0400

    updated for decision text on updating /etc/dmrgateway

--- binupdate.sh
+++ binupdate.sh
@@ -3,19 +3,19 @@
 #  This is the install script to update  the            #
 #  DMRGateway in /usr/local/bin  by VE3RD		#
 #							#
-#  VE3RD                                    2020-06-24  #
+#  VE3RD                                    2021-05-01  #
 #########################################################
 
 sudo mount -o remount,rw /
 # cp ./Extras/dmrgateway.service /usr/local/sbin/
 
-sed -i '/use_colors = /c\use_colors = ON' ~/.dialogrc
-sed -i '/screen_color = /c\screen_color = (WHITE,BLUE,ON)' ~/.dialogrc
-sed -i '/title_color = /c\title_color = (YELLOW,RED,ON)' ~/.dialogrc
 echo -e '\e[1;44m'
 clear
 echo "Starting Binary Install"
 
+echo ""
+echo "If this is a first time install of DMRGateway-4 then say Yes to the /etc/dmrgateway file update"
+echo "If this is an Update of DMRGateway-4 then say No"
 echo ""
 while true; do
     read -p "Do you wish to update your /etc/dmrgateway file?" yn
