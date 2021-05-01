e16ce3f6bf3e1b174db41076339dffb60ee4c802
root <root@pi-star>
2021-05-01 10:13:56 -0400

    Progress Update - Just about Ready

--- GitVersion.h
+++ GitVersion.h
@@ -1,1 +1,1 @@
-const char *gitversion = "a2ea67b36fcc125c6dd5a7927fc1ba0a8a60b265";
+const char *gitversion = "5be6d2a389f5391be95aa24f34b95278b3492b15";
--- binupdate.sh
+++ binupdate.sh
@@ -7,7 +7,25 @@
 #########################################################
 
 sudo mount -o remount,rw /
- cp ./Extras/dmrgateway.service /usr/local/sbin/
+# cp ./Extras/dmrgateway.service /usr/local/sbin/
+
+sed -i '/use_colors = /c\use_colors = ON' ~/.dialogrc
+sed -i '/screen_color = /c\screen_color = (WHITE,BLUE,ON)' ~/.dialogrc
+sed -i '/title_color = /c\title_color = (YELLOW,RED,ON)' ~/.dialogrc
+echo -e '\e[1;44m'
+clear
+echo "Starting Binary Install"
+
+echo ""
+while true; do
+    read -p "Do you wish to update your /etc/dmrgateway file?" yn
+    case $yn in
+        [Yy]* ) cp ./DMRGateway.ini /etc/dmrgateway ; break;;
+        [Nn]* ) break ;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 
 #Check for NetMode
 NetMode=$(sudo sed -nr "/^\[General\]/ { :l /^NetMode[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" /etc/dmrgateway)
@@ -22,5 +40,9 @@ sudo cp ./DMRGateway /usr/local/bin/
 sudo dmrgateway.service start
 sleep 2
 sudo mmdvmhost.service restart
+sed -i '/use_colors = /c\use_colors = OFF' ~/.dialogrc
 
+echo -e '\e[\033[40m'
+
+clear
 
